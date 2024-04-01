b6f77bc1666b42765bf03f4c0db3f47d7e888ebc
Kunkuma Geeth <kunkumagp@gmail.com>
2024-04-01 16:30:18 +0530

    Integrating With HubSpot I: Foundations - Done

--- index.js
+++ index.js
@@ -1,71 +1,119 @@
 const express = require('express');
 const axios = require('axios');
+require('dotenv').config()
 const app = express();
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+const BASEAPI = process.env.BASEAPI;
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PARAMS = 'limit=10&properties=name&properties=year&properties=genres&archived=false';
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+app.get('/', async (req, res) => {
+    const movies = `${BASEAPI}?${PARAMS}`;
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     }
     try {
-        const resp = await axios.get(contacts, { headers });
+        const resp = await axios.get(movies, { headers });
         const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+        console.log(data);
+        res.render('movies', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum', data });
     } catch (error) {
         console.error(error);
     }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
+app.get('/add', async (req, res) => {
+    res.render('add_movies', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
+});
+
+app.post('/add', async (req, res) => {
+    const add = {
         properties: {
-            "favorite_book": req.body.newVal
+            "name": req.body.movieName,
+            "year": req.body.releasedYear,
+            "genres": req.body.movieGenre,
         }
     }
 
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
+    const addMovie = `${BASEAPI}`;
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     };
 
     try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
+        await axios.post(addMovie, add, { headers } );
+        res.redirect('/');
+    } catch(err) {
+        console.error(err);
+    }
+});
+
+app.get('/movie-update', async (req, res) => {
+    const movies = `${BASEAPI}?${PARAMS}`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(movies, { headers });
+        const data = resp.data.results;
+        res.render('update_movie', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum', data });      
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+app.get('/update-cobj', async (req, res) => {
+
+    const id = req.query.ObjectId;
+    const getMovie = `${BASEAPI}/${id}?properties=name,year,genres`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        const response = await axios.get(getMovie, { headers });
+        const data = response.data;
+
+        res.render('edit_movie', {name: data.properties.name, year: data.properties.year, genre: data.properties.genres});
+        
+    } catch(err) {
+        console.error(err);
+    }
+});
+
+app.post('/update-cobj', async (req, res) => {
+    
+    const update = {
+        properties: {
+            "name": req.body.movieName,
+            "year": req.body.releasedYear,
+            "genres": req.body.movieGenre,
+        }
+    }
+
+    const id = req.query.ObjectId;
+    const updateMovie = `${BASEAPI}/${id}`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try { 
+        await axios.patch(updateMovie, update, { headers } );
+        res.redirect('/movie-update');
     } catch(err) {
         console.error(err);
     }
 
 });
-*/
 
-
-// * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
